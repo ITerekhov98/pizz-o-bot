01cded0deb5249c8c380ba261548c2f94e57fc08
ITerekhov98 <iterekhov98@gmail.com>
2022-09-08 15:43:48 +0300

    Add Facebook webhook

---
+++ fb_bot.py
@@ -0,0 +1,64 @@
+import os
+
+import requests
+from flask import Flask, request
+from environs import Env
+ 
+app = Flask(__name__)
+env = Env()
+env.read_env()
+FACEBOOK_TOKEN = env.str("PAGE_ACCESS_TOKEN")
+VERIFY_TOKEN = env.str("VERIFY_TOKEN")
+
+@app.route('/', methods=['GET'])
+def verify():
+    """
+    При верификации вебхука у Facebook он отправит запрос на этот адрес. На него нужно ответить VERIFY_TOKEN.
+    """
+    
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            print(0)
+            return "Verification token mismatch", 403
+        print(1)
+        return request.args["hub.challenge"], 200
+    return VERIFY_TOKEN, 200
+    
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    """
+    Основной вебхук, на который будут приходить сообщения от Facebook.
+    """
+    data = request.get_json()
+    if data["object"] == "page":
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
+                if messaging_event.get("message"):
+                    sender_id = messaging_event["sender"]["id"]
+                    recipient_id = messaging_event["recipient"]["id"]
+                    message_text = messaging_event["message"]["text"]
+                    send_message(sender_id, message_text)
+    return "ok", 200
+
+
+def send_message(recipient_id, message_text):
+    params = {"access_token": FACEBOOK_TOKEN}
+    headers = {"Content-Type": "application/json"}
+    request_content = {
+        "recipient": {
+            "id": recipient_id
+        },
+        "message": {
+            "text": message_text
+        }
+    }
+    response = requests.post(
+        "https://graph.facebook.com/v2.6/me/messages",
+        params=params, headers=headers, json=request_content
+    )
+    response.raise_for_status()
+
+if __name__ == '__main__':
+    app.run(debug=True)
